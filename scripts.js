536ad7bcff97c3876cdcaeef21817c8c05116298
WillieXia <51302855+WillieXia@users.noreply.github.com>
2019-11-03 01:15:58 -0500

    final modifications

--- scripts.js
+++ scripts.js
@@ -1,116 +1,8 @@
-// request.onload = function() {
-// 	var randomInfo = request.response;
-// 	console.log(randomInfo);
-// }
-
-//var website = "http://jService.io/api/clues?id="
-
-function randomGenerator(){
-	var count = document.getElementById("randominput").value;
-	var website = "http://jservice.io/api/random?count=" + count;
-	$.ajax({
-		type: "GET",
-		url: website,
-		dataType: "json",
-		success: function(responseData,status)
-		{
-			var output = '<div class= "randomvalues">';
-			if(count >100){
-				output+='<h2>Only 100 clues can be shown at a time</h2>';
-			}
-			$.each(responseData, function( index, value ) {
-				var answer = "'"+(value.answer)+"'";
-				var airdate = "'"+(value.airdate)+"'";
-				output += '<h4>'+value.question+'</h4>';
-  				output += '<button onclick="showAnswer('+answer+')">Show Answer</button>';
-  				output += '<button onclick="showAirdate('+airdate+')">Show Airdate</button>';
-			});
-			output += '</div>'
-			$('div.randomvalues').html(output);
-		}
-	})
-}
-function dateGeneration(){
-	var start = document.getElementById("startinput");
-	var end = document.getElementById("endinput");
-	var website = "http://jservice.io/api/clues?min_date="+start.value+"&max_date="+end.value;
-	$.ajax({
-		type: "GET",
-		url: website,
-		dataType: "json",
-		success: function(responseData,status)
-		{
-			var output = '<div class= "values">';
-			if(responseData.length==100){
-				output += '<h2 class="resultDesc">The search range entered reached max search count (100)</h2>';
-				output += '<h3 class="resultDesc">Try constraining the date range</h3><br>'
-			}
-			else if(responseData.length==0){
-				output += '<h4 class="resultDesc">Sorry, the range entered is incorrect/has no questions</h4><br>';
-			}
-			else{
-				output += '<h4 class="resultDesc">The range entered resulted in '+responseData.length+' questions</h4><br>';
-			}
-			console.log(responseData);
-			
-			$.each(responseData, function( index, value ) {
-				var answer = "'"+(value.answer)+"'";
-				var airdate = "'"+(value.airdate)+"'";
-				output += '<h4>' + ' Value: ' + value.value + " | Question: "+ value.question + '</h4>';
-  				output += '<button onclick="showAnswer('+answer+')">Show Answer</button>';
-  				output += '<button onclick="showAirdate('+airdate+')">Show Airdate</button>';
-			});
-			output += '</div>';
-			$('div.values').html(output);
-		},
-		error: function(breaker)
-		{
-			breaker=true;
-		}
-	})
-}
-
-function showAnswer(answer){
-	answer = "Answer: " + answer;
-	alert(answer);
-}
-
-function showAirdate(airdate){
-	airdate = "Answer: " + airdate;
-	alert(airdate);
-}
-
-function levelGenerator(){
-	var start = 0;
-	var count = document.getElementById("numberinput").value;
-	var level = document.getElementById("levelinput").value;
-	var website = "http://jservice.io/api/clues?value=" + level;
-	$.ajax({
-		type: "GET",
-		url: website,
-		dataType: "json",
-		success: function(responseData,status)
-		{
-			var output = '<div class= "level">';
-			if(count >100){
-				output+='<h2>Only 100 clues can be shown at a time</h2>';
-			}
-			$.each(responseData, function( index, value ) {
-				var answer = "'"+(value.answer)+"'";
-				var airdate = "'"+(value.airdate)+"'";
-				output += '<h4>'+value.question+'</h4>';
-  				output += '<button onclick="showAnswer('+answer+')">Show Answer</button>';
-  				output += '<button onclick="showAirdate('+airdate+')">Show Airdate</button>';
-  				start++;
-  				if(start == count || start == 100){
-  					output += '</div>'
-  					$('div.levelvalues').html(output);
-  				}
-			});
-		}
-	})
-}
-
+//This is the function that searches for categories that contain keywords in the input.
+//This function can easily be optimized, as the runtime of it is O(n*m), where n
+//is the number of words and m is the length of each word. This results in a long
+//run time, and possibly crashes. Due to lack of experience with javascript and time, I wasn't
+//able to implement a efficient data structure.
 function categoryGeneration(){
 	var dataid = [];
 	var categorytitle = [];
@@ -135,7 +27,7 @@ function categoryGeneration(){
 		offset += 100;
 		website = "http://jservice.io/api/categories?count=100&offset="+offset;
 	}
-	var output = '<div id=categoryval>';
+	var output = '<div id="categoryval">';
 	for (i=0;i<categorytitle.length;i++){
 		output += '<button onclick="showCategoryInfo('+dataid[i]+')">'+categorytitle[i]+' ID = '+dataid[i]+')</button>';
 	}
@@ -143,6 +35,10 @@ function categoryGeneration(){
 	//alert(output);
   	$('div.categoryvalues').html(output);
 }
+
+//This is a helper function that works in conjunction with the category search function
+//When the output from the category search is shown, they have the option of calling this function
+//to further give details on the questions within the category.
 function showCategoryInfo(id){
 	var website = 'http://jService.io/api/category?id=';
 	website += id;
@@ -166,6 +62,41 @@ function showCategoryInfo(id){
 	})	
 }
 
+//This is the function that searches by questions based on their value.
+function levelGenerator(){
+	var start = 0;
+	var count = document.getElementById("numberinput").value;
+	var level = document.getElementById("levelinput").value;
+	var website = "http://jservice.io/api/clues?value=" + level;
+	$.ajax({
+		type: "GET",
+		url: website,
+		dataType: "json",
+		success: function(responseData,status)
+		{
+			var output = '<div class= "level">';
+			output += '<h1>Value: '+level+'<h1>';
+			if(count >100){
+				output+='<h2>Only 100 clues can be shown at a time</h2>';
+			}
+			$.each(responseData, function( index, value ) {
+				var answer = "'"+(value.answer)+"'";
+				var airdate = "'"+(value.airdate)+"'";
+				output += '<h4>'+value.question+'</h4>';
+  				output += '<button onclick="showAnswer('+answer+')">Show Answer</button>';
+  				output += '<button onclick="showAirdate('+airdate+')">Show Airdate</button>';
+  				start++;
+  				if(start == count || start == 100){
+  					output += '</div>'
+  					$('div.levelvalues').html(output);
+  				}
+			});
+		}
+	})
+}
+
+//This is the function that works the general search method. If values are given in each
+//input, they are added to the website url and processed using the api functionalities.
 function questionSearch(){
 	var website = 'http://jService.io/api/clues?';
 	if(document.getElementById("evestart").value!=""){
@@ -206,3 +137,85 @@ function questionSearch(){
 	})
 }
 
+//This is the function that processes the date search method by taking in 2 dates
+//and utilizes the api function to retrieve data.
+function dateGeneration(){
+	var start = document.getElementById("startinput");
+	var end = document.getElementById("endinput");
+	var website = "http://jservice.io/api/clues?min_date="+start.value+"&max_date="+end.value;
+	$.ajax({
+		type: "GET",
+		url: website,
+		dataType: "json",
+		success: function(responseData,status)
+		{
+			var output = '<div class= "values">';
+			if(responseData.length==100){
+				output += '<h2 class="resultDesc">The search range entered reached max search count (100)</h2>';
+				output += '<h3 class="resultDesc">Try constraining the date range</h3><br>'
+			}
+			else if(responseData.length==0){
+				output += '<h4 class="resultDesc">Sorry, the range entered is incorrect/has no questions</h4><br>';
+			}
+			else{
+				output += '<h4 class="resultDesc">The range entered resulted in '+responseData.length+' questions</h4><br>';
+			}
+			console.log(responseData);
+			
+			$.each(responseData, function( index, value ) {
+				var answer = "'"+(value.answer)+"'";
+				var airdate = "'"+(value.airdate)+"'";
+				output += '<h4>' + ' Value: ' + value.value + " | Question: "+ value.question + '</h4>';
+  				output += '<button onclick="showAnswer('+answer+')">Show Answer</button>';
+  				output += '<button onclick="showAirdate('+airdate+')">Show Airdate</button>';
+			});
+			output += '</div>';
+			$('div.values').html(output);
+		},
+		error: function(breaker)
+		{
+			breaker=true;
+		}
+	})
+}
+
+//This is the function that processes the random command.
+//It takes in a count and utilizes the api function to retrieve data.
+function randomGenerator(){
+	var count = document.getElementById("randominput").value;
+	var website = "http://jservice.io/api/random?count=" + count;
+	$.ajax({
+		type: "GET",
+		url: website,
+		dataType: "json",
+		success: function(responseData,status)
+		{
+			var output = '<div class= "randomvalues">';
+			if(count >100){
+				output+='<h2>Only 100 clues can be shown at a time</h2>';
+			}
+			$.each(responseData, function( index, value ) {
+				var answer = "'"+(value.answer)+"'";
+				var airdate = "'"+(value.airdate)+"'";
+				output += '<h4>'+value.question+'</h4>';
+  				output += '<button onclick="showAnswer('+answer+')">Show Answer</button>';
+  				output += '<button onclick="showAirdate('+airdate+')">Show Airdate</button>';
+			});
+			output += '</div>'
+			$('div.randomvalues').html(output);
+		}
+	})
+	document.getElementById("startinput").scrollIntoView();
+}
+
+
+//Helper function for showing answers and airdate
+function showAnswer(answer){
+	answer = "Answer: " + answer;
+	alert(answer);
+}
+
+function showAirdate(airdate){
+	airdate = "Answer: " + airdate;
+	alert(airdate);
+}
